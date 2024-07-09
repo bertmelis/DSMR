cf681aff38414539564ee67b08fce968d9184046
Bert Melis <bertmelis@users.noreply.github.com>
2024-07-09 12:51:34 +0200

    test full (wip)

--- test/test_parser/test_parser.cpp
+++ test/test_parser/test_parser.cpp
@@ -5,7 +5,7 @@
 void setUp() {}
 void tearDown() {}
 
-void test_uint32_t() {
+void test_Short() {
   const char msg[] =
     "/KFM5KAIFA-METER\r\n"
     "\r\n"
@@ -21,11 +21,116 @@ void test_uint32_t() {
   ParseResult<void> res = P1Parser::parse(&myData, msg, lengthof(msg));
 
   TEST_ASSERT_FALSE(res.err);
+  TEST_ASSERT_EQUAL_STRING("KFM5KAIFA-METER", myData.identification);
   TEST_ASSERT_EQUAL_UINT32(318, myData.power_delivered.int_val());
+  TEST_ASSERT_FLOAT_WITHIN(0.1, 0.318, myData.power_delivered.val());
+}
+
+void test_full() {
+const char msg[] =
+    "/KFM5KAIFA-METER\r\n"
+    "\r\n"
+    "1-3:0.2.8(40)\r\n"
+    "0-0:1.0.0(150117185916W)\r\n"
+    "0-0:96.1.1(0000000000000000000000000000000000)\r\n"
+    "1-0:1.8.1(000671.578*kWh)\r\n"
+    "1-0:1.8.2(000842.472*kWh)\r\n"
+    "1-0:2.8.1(000000.000*kWh)\r\n"
+    "1-0:2.8.2(000000.000*kWh)\r\n"
+    "0-0:96.14.0(0001)\r\n"
+    "1-0:1.7.0(00.333*kW)\r\n"
+    "1-0:2.7.0(00.000*kW)\r\n"
+    "0-0:17.0.0(999.9*kW)\r\n"
+    "0-0:96.3.10(1)\r\n"
+    "0-0:96.7.21(00008)\r\n"
+    "0-0:96.7.9(00007)\r\n"
+    "1-0:99.97.0(1)(0-0:96.7.19)(000101000001W)(2147483647*s)\r\n"
+    "1-0:32.32.0(00000)\r\n"
+    "1-0:32.36.0(00000)\r\n"
+    "0-0:96.13.1()\r\n"
+    "0-0:96.13.0()\r\n"
+    "1-0:31.7.0(001*A)\r\n"
+    "1-0:21.7.0(00.332*kW)\r\n"
+    "1-0:22.7.0(00.000*kW)\r\n"
+    "0-1:24.1.0(003)\r\n"
+    "0-1:96.1.0(0000000000000000000000000000000000)\r\n"
+    "0-1:24.2.1(150117180000W)(00473.789*m3)\r\n"
+    "0-1:24.4.0(1)\r\n"
+    "!6F4A\r\n";
+
+  ParsedData<
+    /* String */ identification,
+    /* String */ p1_version,
+    /* String */ timestamp,
+    /* String */ equipment_id,
+    /* FixedValue */ energy_delivered_tariff1,
+    /* FixedValue */ energy_delivered_tariff2,
+    /* FixedValue */ energy_returned_tariff1,
+    /* FixedValue */ energy_returned_tariff2,
+    /* String */ electricity_tariff,
+    /* FixedValue */ power_delivered,
+    /* FixedValue */ power_returned,
+    /* FixedValue */ electricity_threshold,
+    /* uint8_t */ electricity_switch_position,
+    /* uint32_t */ electricity_failures,
+    /* uint32_t */ electricity_long_failures,
+    /* String */ electricity_failure_log,
+    /* uint32_t */ electricity_sags_l1,
+    /* uint32_t */ electricity_sags_l2,
+    /* uint32_t */ electricity_sags_l3,
+    /* uint32_t */ electricity_swells_l1,
+    /* uint32_t */ electricity_swells_l2,
+    /* uint32_t */ electricity_swells_l3,
+    /* String */ message_short,
+    /* String */ message_long,
+    /* FixedValue */ voltage_l1,
+    /* FixedValue */ voltage_l2,
+    /* FixedValue */ voltage_l3,
+    /* FixedValue */ current_l1,
+    /* FixedValue */ current_l2,
+    /* FixedValue */ current_l3,
+    /* FixedValue */ power_delivered_l1,
+    /* FixedValue */ power_delivered_l2,
+    /* FixedValue */ power_delivered_l3,
+    /* FixedValue */ power_returned_l1,
+    /* FixedValue */ power_returned_l2,
+    /* FixedValue */ power_returned_l3,
+    /* uint16_t */ gas_device_type,
+    /* String */ gas_equipment_id,
+    /* uint8_t */ gas_valve_position,
+    /* TimestampedFixedValue */ gas_delivered,
+    /* uint16_t */ thermal_device_type,
+    /* String */ thermal_equipment_id,
+    /* uint8_t */ thermal_valve_position,
+    /* TimestampedFixedValue */ thermal_delivered,
+    /* uint16_t */ water_device_type,
+    /* String */ water_equipment_id,
+    /* uint8_t */ water_valve_position,
+    /* TimestampedFixedValue */ water_delivered,
+    /* uint16_t */ sub_device_type,
+    /* String */ sub_equipment_id,
+    /* uint8_t */ sub_valve_position,
+    /* TimestampedFixedValue */ sub_delivered
+  > myData;
+
+  ParseResult<void> res = P1Parser::parse(&myData, msg, lengthof(msg));
+
+  TEST_ASSERT_FALSE(res.err);
+  // test each type only once
+  TEST_ASSERT_EQUAL_STRING("150117185916W", myData.timestamp);
+  TEST_ASSERT_EQUAL_STRING("0001", myData.electricity_tariff);
+  TEST_ASSERT_EQUAL_UINT32(671578, myData.energy_delivered_tariff1.int_val());  // part of FixedValue
+  TEST_ASSERT_FLOAT_WITHIN(0.1, 671.578, myData.energy_delivered_tariff1.val());  // part of FixedValue
+  TEST_ASSERT_EQUAL_UINT8(1, myData.electricity_switch_position);
+  TEST_ASSERT_EQUAL_UINT32(8, myData.electricity_failures);
+  TEST_ASSERT_EQUAL_UINT16(3, myData.gas_device_type);
+  TEST_ASSERT_EQUAL_STRING("150117180000W", myData.gas_delivered.timestamp());
+  TEST_ASSERT_FLOAT_WITHIN(0.1, 473.789, myData.gas_delivered.val());
 }
 
 int main() {
   UNITY_BEGIN();
-  RUN_TEST(test_uint32_t);
+  RUN_TEST(test_Short);
+  UN_TEST(test_Full);
   return UNITY_END();
 }
